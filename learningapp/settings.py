a73ab0eac414a587737c221692688ea214b4520e
Jim Barnes <jimbarnesdeveloper@gmail.com>
2022-10-26 12:58:54 -0400

    Reconfigured to that files get moved using collectstatic

--- learningapp/settings.py
+++ learningapp/settings.py
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'learningapp/dist'
+]
+
 STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
